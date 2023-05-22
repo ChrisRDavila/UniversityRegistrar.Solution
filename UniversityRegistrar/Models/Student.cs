7c3c89f4e71ee9f9fd0deea726f985d33dd67550
ChrisRDavila <crossdavila@gmail.com>
2023-05-22 15:40:17 -0700

    added models, context file and student controller/views for index and create
    Co-authored-by: Max Betich <MaxBetich@users.noreply.github.com>

---
+++ UniversityRegistrar/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UniversityRegistrar.Models
+
+{
+  public class Student
+  {
+    public int StudentId { get; set; }
+    public string StudentName { get; set; }
+    public DateTime Enrollment { get; set; }
+  }
+}
--- UniversityRegistrar/Program.cs
+++ UniversityRegistrar/Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using ProjectName.Models;
+using UniversityRegistrar.Models;
 
-namespace ProjectName
+namespace UniversityRegistrar
 {
   class Program
   {
@@ -14,7 +14,7 @@ namespace ProjectName
 
       builder.Services.AddControllersWithViews();
 
-      builder.Services.AddDbContext<ProjectNameContext>(
+      builder.Services.AddDbContext<UniversityRegistrarContext>(
                         dbContextOptions => dbContextOptions
                           .UseMySql(
                             builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
