e139590e1f9572963d0f448b0d3bd92d74fee754
ChrisRDavila <crossdavila@gmail.com>
2023-05-23 11:07:42 -0700

    used data annotations to change StudentName property from nullable to non nullable

--- UniversityRegistrar/Models/Student.cs
+++ UniversityRegistrar/Models/Student.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityRegistrar.Models
 
@@ -7,6 +8,8 @@ namespace UniversityRegistrar.Models
   public class Student
   {
     public int StudentId { get; set; }
+    [Required(ErrorMessage = "The students name field can't be empty!")]
+
     public string StudentName { get; set; }
     public DateTime Enrollment { get; set; }
     public List<StudentCourse> JoinEntities { get; }
