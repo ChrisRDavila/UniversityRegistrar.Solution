53c0293c7cdda73fedd18bb1592d5457bd32102b
ChrisRDavila <crossdavila@gmail.com>
2023-05-23 13:08:25 -0700

    WIP setting up dept cont

---
+++ UniversityRegistrar/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class DepartmentController : Controller
+  {
+    private readonly UniversityRegistrar _db;
+    public Department
+  }
+}
